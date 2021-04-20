6485fbb8486c8b6d6feea6b2ecc2df88a080bf18
Rabz92 <rabbiailyas92@gmail.com>
2021-04-20 17:00:18 +0200

    update

--- D6.js
+++ D6.js
@@ -5,45 +5,101 @@
     let firstHeading = document.getElementById('mainTitle')
     
     if(firstHeading.innerText = buttonValue){      
-    firstHeading.innerText = 'Smart Shopping'
+        firstHeading.innerText = 'Smart Shopping'
+        firstHeading.style.color = "gold"
     }
     else{
+        // firstHeading.style.color = "gold"
         firstHeading.innerText =  'Smart OneStopShop'
+        firstHeading.style.color = "black"
+       
         // let colorChange = firstHeading.innerText
         // colorChange.style.color = "gold"       
     }
     buttonValue = !buttonValue
    
 }
-
+let back = false
 const changePageBackgroundColor = function(){
-    document.body.style.backgroundColor = "lightgrey"
+    if(back === true){
+        document.body.style.backgroundColor = "white"
+    }
+    else{
+        document.body.style.backgroundColor = "lightgrey"
+    }  
+    back = !back
 }
 
+let foot = false
 const changeFooterAddress = function(){
     let footerAddress = document.getElementById('footer')  
-    footerAddress.innerText = 'Visit us at: Fake Address, str no. fake, postal# fake, fake, fake'
+    if(foot === true){
+        footerAddress.innerText = 'myNew Adresss Street no. 45, 78021, Hamburg, Germany'
+    }
+        else{
+            footerAddress.innerText = 'Visit us at: Fake Address, str no. fake, postal# fake, fake, fake'        
+    }
+    foot = !foot
 }
 
+let css = false
 const addCssClass = function(){
     let newCssClass = document.querySelectorAll('.link')  
         for(let i=0; i<newCssClass.length; i++){
-            newCssClass[i].classList.add("amazonChange")        
-    }   
+            if(css === true){
+                newCssClass[i].classList.remove("amazonChange")
+            }
+            else{
+                newCssClass[i].classList.add("amazonChange")
+            }
+                    
+    }  
+    css = !css
 }
 
-const toggleClass = function(){
-    let imageToggle = document.querySelectorAll("img")
-    for(let i=0; i<imageToggle.length; i++){
-        if(imageToggle){
-            imageToggle[i].style.display = "none"
+// const toggleClass = function(){
+//     let imageToggle = document.querySelectorAll("img")
+//     for(let i=0; i<imageToggle.length; i++){
+//          if(imageToggle){
+//             imageToggle[i].style.display = "none"
 
-        }
+//         }
 
        
+//     }
+// }
+let toggle = false
+const toggleClass = function () {
+    //
+    let imageToggle = document.querySelectorAll("img")
+    
+    for(let i=0; i<imageToggle.length; i++){
+        if(toggle === true){
+            imageToggle[i].style.display = 'none'
     }
-} 
+    else{
+        imageToggle[i].style.display = 'block'
+    }
+  }
+  toggle = !toggle
+}
 
+
+// const toggleClass = function(){
+//     let imageToggle = document.getElementsByClassName(".Image")
+//     for(let i=0; i<imageToggle.length; i++){  
+//             if(imageToggle[i].style.display === "block"){
+//                 imageToggle[i].style.display = "none"
+//             }
+//             else{
+//                 imageToggle[i].style.display = "block" 
+//             }
+            
+                          
+       
+//     }
+// }
+let color = false
 const priceColor = function(){
     let diffColor = document.getElementsByClassName("price")
     let colorArray = ["red", "orange", "yellow", "green", "blue"]
@@ -51,9 +107,15 @@ const priceColor = function(){
  
         for(let i=0; i<diffColor.length; i++){
             for(let j=0; j<colorArray.length; j++){
-                diffColor[i].style.color = colorArray[i]
+                if(color === true){
+                    diffColor[i].style.color = colorArray[i]
+                }
+                else{
+                    diffColor[i].style.color = 'black'
+                }
+                
             }
         }  
-    
+    color = !color
 }
 
